fbb2188fd59c295954948d717a1b0bb62e8c5025
CampbellHarry <hthebeast7@gmail.com>
2024-10-16 13:55:33 +0100

    Refactor Convex configuration and UI components, fix route matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/application/(.*)'])
+const isProtectedRoute = createRouteMatcher(['/application(.*)'])
 
 export default clerkMiddleware((auth, req) => {
  if (isProtectedRoute(req)) auth().protect()
