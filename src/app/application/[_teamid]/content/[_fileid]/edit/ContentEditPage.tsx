6e4d3065b6a011837983db2886ab7f693db5b430
CampbellHarry <hthebeast7@gmail.com>
2024-11-26 00:55:33 +0000

    Add Tailwind CSS typography plugin; update package.json and package-lock.json, and adjust RichTextViewer and RichTextEditor components for improved styling

--- src/app/application/[_teamid]/content/[_fileid]/edit/ContentEditPage.tsx
+++ src/app/application/[_teamid]/content/[_fileid]/edit/ContentEditPage.tsx
@@ -1008,7 +1008,7 @@ function MessageInputter({authorid, contentid, teamid}: any) {
 function RichTextViewer({ content }: { content: string }) {
     return (
         <div className='flex flex-col gap-1'>
-            <div className='prose prose-headings:text-blue-600 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg text-sm font-medium break-all'>
+            <div className='prose prose-headings:text-blue-600 prose-a:text-blue-400 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg text-sm font-medium break-all'>
                 <div className='content prose' dangerouslySetInnerHTML={{ __html: content }} />
             </div>
         </div>
