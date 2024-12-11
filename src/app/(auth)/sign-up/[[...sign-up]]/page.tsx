d6662e0e323face3ab8977d2b1ccac0651fb6a91
CampbellHarry <hthebeast7@gmail.com>
2024-12-11 00:15:44 +0000

    Enhance sign-in and sign-up pages with image rotation and color transitions; update image sources and improve layout structure

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -12,16 +12,14 @@ export default function Page() {
   const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
   const [rotation, setRotation] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [imageState, setImageState] = useState('entering');
 
   const colourgradientsandimages = [
     { color: '#00ffff', image: '/IMG_6128.png' }, // Cyan
-    { color: '#0088ff', image: '/IMG_6128.png' }, // Ocean Blue
-    { color: '#4b0082', image: '/IMG_6128.png' }, // Indigo
-    { color: '#6a5acd', image: '/IMG_6128.png' }, // Slate Blue
-    { color: '#7fffd4', image: '/IMG_6128.png' }, // Aquamarine
-    { color: '#5f9ea0', image: '/IMG_6128.png' }, // Cadet Blue
-    { color: '#483d8b', image: '/IMG_6128.png' }, // Dark Slate Blue
-    { color: '#8a2be2', image: '/IMG_6128.png' }, // Blue Violet
+    { color: '#0088ff', image: '/icons/IMG_6489.png' }, // Ocean Blue
+    { color: '#4b0082', image: '/icons/IMG_6490.png' }, // Indigo
+    { color: '#6a5acd', image: '/icons/IMG_6491.png' }, // Slate Blue
   ];
 
   useEffect(() => {
@@ -40,14 +38,22 @@ export default function Page() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setRotation((prev) => prev + 90);
-      setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colourgradientsandimages.length);
-    }, 2000);
+      setImageState('leaving'); 
+
+      setTimeout(() => {
+        setRotation((prev) => prev + 90);
+        setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colourgradientsandimages.length);
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % colourgradientsandimages.length);
+
+        setImageState('entering');
+      }, 500);
+    }, 2500);
 
     return () => clearInterval(interval);
   }, [colourgradientsandimages.length]);
 
   const currentColor = colourgradientsandimages[currentColorIndex].color;
+  const currentImage = currentImageIndex === 4 ? '/IMG_6128.png' : colourgradientsandimages[currentImageIndex].image;
 
   return (
     <body className='overflow-hidden'>
@@ -55,8 +61,10 @@ export default function Page() {
       <main className="flex flex-col md:flex-row justify-between items-start h-screen bg-background">
         <div className='mx-auto md:px-10 h-screen flex flex-col w-full'>
           <div className='w-full h-20 md:px-0 px-3 py-3 flex items-center justify-between'>
-            <img src="/wordmarks/light-removebg-preview.png" className='w-auto h-10'></img>
-            <div className='flex flex-row gap-2 items-center'>
+            <a href='/'>
+              <img src="/wordmarks/light-removebg-preview.png" className='w-auto h-10'></img>
+            </a>
+            <div className='flex flex-row gap-4 items-center'>
               <p className='text-muted-foreground text-sm'>Already have an account?</p>
               <Link href='/sign-in'>
                 <Button variant='secondary'>Log in</Button>
@@ -65,17 +73,26 @@ export default function Page() {
           </div>
           <div className='w-full rounded-t-2xl bg-gradient-to-t flex items-center flex-col h-full from-background to-[#1f1f23] p-8'>
             <div className='flex flex-col gap-4 items-center md:mt-[3%]'>
-              <div className='relative w-16 h-16 rounded-full bg-transparent flex items-center justify-center'>
-              <div
-                className='w-16 h-16 rounded-xl z-20 absolute flex items-center justify-center'
-                id='spinzoner'
-                style={{
+              <div className='relative w-20 h-20 overflow-hidden bg-transparent flex items-center justify-center'>
+                <div
+                  className='w-16 h-16 rounded-xl overflow-hidden z-20 m-4 absolute flex items-center justify-center'
+                  id='spinzoner'
+                  style={{
                   transform: `rotate(${rotation}deg)`,
-                  backgroundColor: rotation % 360 === 0 ? '#1f1f23' : currentColor,
+                  backgroundColor: rotation % 360 === 0 ? '#111114' : currentColor,
                   transition: 'transform 0.5s, background-color 0.5s',
-                }}
-              />
-              <img src='/IMG_6128.png' className='w-full p-1.5 z-50' id='spinzonerimg'></img>
+                  }}
+                />
+                <div className='w-14 h-14 overflow-hidden flex items-center justify-center'>
+                  <img
+                    src={currentImage}
+                    className={`w-14 h-14 object-contain z-50 p-1.5 transition-transform duration-500 ease-in-out ${
+                      imageState === 'entering'
+                      ? 'translate-x-[0%]'
+                      : 'translate-x-[100%]'
+                    }`}
+                    />
+                </div>
               </div>
               <h1 className='text-[40px] md:text-[63px] font-semibold text-white'>Create your content</h1>
               <SignUp
