b77d220b7ee58fb3293d2a7c63d5552995d25ef4
CampbellHarry <hthebeast7@gmail.com>
2025-01-01 23:10:10 +0000

    Add GSAP library for enhanced animations; fix environment variable name for AWS access key

--- src/service/aws-ses.js
+++ src/service/aws-ses.js
@@ -5,7 +5,7 @@ import * as nodemailer from "nodemailer";
 const sesClient = new SESClient({
     region: "eu-north-1",
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
 });
